17432d06559b79eeb9c0d1603a62702e7f2b62fd
SaraBuu <sara.aguincha@gmail.com>
2023-11-28 22:45:13 +0000

    added last second of the cycle

--- Lab1/Lab1.ino
+++ Lab1/Lab1.ino
@@ -18,7 +18,7 @@ void setup() {
 }
 
 void loop() {
-  // Cycle of 5 seconds
+  // Cycle of 4 seconds (LEDs)
   for (int i = 0; i < sizeof(LEDs); i++) { // Iterate through LEDs
     digitalWrite(LEDs[i], HIGH); // Turn on LED
     time_now = millis();
@@ -29,6 +29,12 @@ void loop() {
       }
     }    
     digitalWrite(LEDs[i], LOW); // Turn off LED
+  }
+  // Last second of the 5 second cycle
+  while(millis() < time_now + second){
+      while (isFrozen) { // Check pause flag
+        delay(10); // Delay to avoid excessive CPU 
+      }
   }
 }
 
