674f2c7a30bb375c0c78086085e0ba379d3d586d
franciscomcsousa <francisco.m.c.sousa@gmail.com>
2023-12-01 15:26:56 +0000

    Who brought who brough, it was the sweet drop

--- Lab2/master/master.ino
+++ Lab2/master/master.ino
@@ -1,20 +1,58 @@
 #include <Wire.h>
+
+int potentiometerSensor = 0;
+int potentiometerMapped = 0;
+int potentiometerPin = A3;
+
+
+int lightSensor = 0;
+int lightMapped = 0;
+int lightPin = A1;
+
+int lightMin = 0;
+int lightMax = 1023;
+
+int tempSensor = 0;
+int tempMapped = 0;
+int tempPin = A0;
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);           // start serial for output
   Wire.begin();
+  pinMode(tempPin, INPUT);
+  pinMode(lightPin, INPUT);
+  pinMode(potentiometerPin, INPUT);
 }
 
-int sensorValue = 0;
-int mappedValue = 0;
 
 void loop() {
   // put your main code here, to run repeatedly:
   Wire.beginTransmission(4); // transmit to device #4
-  Wire.write("sensor value is ");        // sends five bytes
-  sensorValue = analogRead(A3);
-  mappedValue = map(sensorValue, 0, 1023, 1, 24);
-  Wire.write(mappedValue);// sends one byte  
+  
+  // Potentiometer section
+  Wire.write("P");        // sends five bytes
+  potentiometerSensor = analogRead(potentiometerPin);
+  potentiometerMapped = map(potentiometerSensor, 0, 1023, 0, 180);
+  Wire.write(potentiometerMapped);// sends one byte 
   Wire.endTransmission();    // stop transmitting
 
-  delay(500);
+  Wire.beginTransmission(4);
+
+  Wire.write("L");
+  lightSensor = analogRead(lightPin);
+  lightMapped = map(lightSensor, lightMin, lightMax, 0, 255);
+  Wire.write(lightMapped);// sends one byte 
+  //Serial.println(lightSensor);
+  Wire.endTransmission();    // stop transmitting
+
+  Wire.beginTransmission(4);
+  Wire.write("T");
+  tempSensor = analogRead(tempPin);
+  tempMapped = (((tempSensor / 1024.0) * 5.0 ) - 0.5 ) * 100;
+  Wire.write(tempMapped);// sends one byte
+
+  Wire.endTransmission();    // stop transmitting
+
+  delay(1000);
 }
--- Lab2/slave/slave.ino
+++ Lab2/slave/slave.ino
@@ -1,25 +1,84 @@
 #include <Wire.h>
+int redPin = 4;
+int bluePin = 6;
+int yellowPin = 5;
+
+unsigned long time_actuator = 0;
+int mappedFrequency = 0;
+
 void setup() {
   // put your setup code here, to run once:
   Wire.begin(4);                // join i2c bus with address #4
   Wire.onReceive(receiveEvent); // register event
   Serial.begin(9600);           // start serial for output
+  pinMode(redPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+  pinMode(yellowPin, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  delay(100);
+  if(millis() > time_actuator + mappedFrequency){
+    digitalWrite(bluePin, LOW);
+  }
+  delay(10);
 }
 
 // function that executes whenever data is received from master
 // this function is registered as an event, see setup()
 void receiveEvent(int howMany)
 {
+  char c = ' ';
   while(1 < Wire.available()) // loop through all but the last
   {
-    char c = Wire.read(); // receive byte as a character
-    Serial.print(c);         // print the character
+    c = Wire.read(); // receive byte as a character
+    //Serial.print(c);         // print the character
   }
   int x = Wire.read();    // receive byte as an integer
-  Serial.println(x);         // print the integer
+  //Serial.println(x);         // print the integer
+
+  switch (c){
+    case 'T':
+      Serial.print("Temperature is ");
+      Serial.println(x);
+      tempActuator(x);
+      break;
+    case 'L':
+      Serial.print("Light is ");
+      Serial.println(x);
+      lightActuator(x);
+      break;
+    case 'P':
+      Serial.print("Potentiometer is ");
+      Serial.println(x);
+      potentiometerActuator(x);
+      break;
+  }
+}
+
+void tempActuator(int temp)
+{
+  if (temp > 18)
+  {
+    digitalWrite(redPin, HIGH);
+  }
+  else
+  {
+    digitalWrite(redPin, LOW);
+  }
+}
+
+void potentiometerActuator(int frequency)
+{
+  mappedFrequency = map(frequency, 0, 180, 200, 2000);
+  time_actuator = millis();
+
+  //Serial.println(mappedFrequency);
+
+  digitalWrite(bluePin, HIGH);
+}
+
+void lightActuator(int intensity)
+{
+  analogWrite(yellowPin, 255 - intensity);
 }
